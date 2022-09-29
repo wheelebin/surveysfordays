44fa76984c260440f3a68b517f33aaf70a370356
wheelebin <joakim.allen@hotmail.com>
2022-09-29 20:25:31 +0200

    Removed unit tests

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: "ts-jest/presets/js-with-ts-esm",
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.ts?$": "ts-jest",
-  },
-  transformIgnorePatterns: ["<rootDir>/node_modules/"],
-};
