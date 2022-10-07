c7d7a95c216b08a3b120fcb845d9cd2cd9c177a5
wheelebin <joakim.allen@hotmail.com>
2022-10-07 15:09:37 +0200

    Added basic seperator

---
+++ src/components/AppSeperator.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as Separator from "@radix-ui/react-separator";
+
+const AppSeperator: React.FC = () => {
+  return <Separator.Root className="h-px bg-black my-2" />;
+};
+
+export default AppSeperator;
