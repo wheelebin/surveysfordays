80f663b0931787a73bbd77f804f890ec9bc9f6fe
Wheelebin <jln.allen95@gmail.com>
2022-12-14 16:01:51 +0100

    fixed undefined class & added className prop

--- src/components/AppCard.tsx
+++ src/components/AppCard.tsx
@@ -3,12 +3,15 @@ import React from "react";
 type Props = {
   children?: React.ReactNode;
   isCurrent?: boolean;
+  className?: string;
 };
 
-const AppButton: React.FC<Props> = ({ children, isCurrent }) => {
+const AppButton: React.FC<Props> = ({ children, isCurrent, className }) => {
   return (
     <div
-      className={`py-5 px-3 shadow rounded-md bg-white ${isCurrent && "py-20"}`}
+      className={`py-5 px-3 shadow rounded-md bg-white ${
+        isCurrent ? "py-20" : ""
+      } ${className}`}
     >
       {children}
     </div>
