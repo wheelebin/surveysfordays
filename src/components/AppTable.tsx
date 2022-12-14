291013e0d8e4e248dc766c0afa75619ca15d8aeb
Wheelebin <jln.allen95@gmail.com>
2022-12-14 16:44:55 +0100

    Added tags to cells in

--- src/components/AppTable.tsx
+++ src/components/AppTable.tsx
@@ -3,9 +3,18 @@ import AppTableHeader from "./AppTableHeader";
 import AppCheckbox from "./AppCheckbox";
 import { boolean, string } from "zod";
 
+const cellValueTag = (text: string) =>
+  text ? (
+    <span className="bg-white border border-slate-200 p-1 rounded-md">
+      {text}
+    </span>
+  ) : (
+    <span className="bg-slate border border-slate-200 p-1 rounded-md">-</span>
+  );
+
 type Props = {
   headerItems: { text: string; id?: string }[];
-  contentItems: [string][];
+  contentItems: string[][][];
 };
 
 const AppTable = ({ headerItems, contentItems }: Props) => {
@@ -22,18 +31,22 @@ const AppTable = ({ headerItems, contentItems }: Props) => {
               <AppTableHeader items={headerItems.map(({ text }) => text)} />
               <tbody className="divide-y divide-gray-200 ">
                 {contentItems.map((row, i) => (
-                  <tr className="hover:bg-gray-100 " key={"row-" + i}>
+                  <tr className="bg-white hover:bg-gray-100" key={"row-" + i}>
                     <td className="px-6 py-4 bg-white">
                       <AppCheckbox />
                     </td>
-                    {row.map((item, i) => (
-                      <td
-                        key={"cell-" + i}
-                        className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800"
-                      >
-                        {item}
-                      </td>
-                    ))}
+                    {row.map((values, i) => {
+                      // TODO Change this stuff man, data structure should be somewhat complete and ready from consumption after being sent from BE
+                      const valuesInternal = values || [undefined];
+                      return (
+                        <td
+                          key={"cell-" + i}
+                          className="px-6 py-4 whitespace-nowrap text-xs font-normal text-gray-800"
+                        >
+                          {valuesInternal.map((value) => cellValueTag(value))}
+                        </td>
+                      );
+                    })}
                   </tr>
                 ))}
               </tbody>
