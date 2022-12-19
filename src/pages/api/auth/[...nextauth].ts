2519e417fe0ae6bc5ecd80ed45e98cc74fc2c217
Wheelebin <jln.allen95@gmail.com>
2022-12-19 19:43:59 +0100

    Added redirect after login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -27,6 +27,9 @@ export const authOptions: NextAuthOptions = {
       }
       return session
     },
+    async redirect({url, baseUrl}) {
+      return `${baseUrl}/surveys`
+    }
   }
 };
 export default NextAuth(authOptions);
