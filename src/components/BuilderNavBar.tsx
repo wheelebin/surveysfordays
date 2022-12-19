bf8e9eb1b6f4a77d6e6bdcbfe041ebda70644e71
Wheelebin <jln.allen95@gmail.com>
2022-12-19 22:11:34 +0100

    replaced preview new tab on click with dialog

--- src/components/BuilderNavBar.tsx
+++ src/components/BuilderNavBar.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { trpc } from "@/utils/trpc";
 import { EyeIcon } from "@heroicons/react/20/solid";
 import AppButton from "@/components/AppButton";
 import surveyApi from "@/api/survey";
+import * as Dialog from "@radix-ui/react-dialog";
+
+const DialogComponent = ({
+  trigger,
+  children,
+}: {
+  trigger: React.ReactNode;
+  children: React.ReactNode[] | React.ReactNode;
+}) => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <Dialog.Root open={open}>
+      <Dialog.Trigger onClick={() => setOpen(true)}>{trigger}</Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-red fixed top-0 left-0 right-0 bottom-0 grid place-items-center overflow-y-scroll">
+          <Dialog.Content className="w-full h-full bg-white p-3">
+            <div className="w-full">
+              <div className="w-full flex justify-end">
+                <AppButton onClick={() => setOpen(false)}>Close</AppButton>
+              </div>
+              {children}
+            </div>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
 
 const AppNavBar = () => {
   const router = useRouter();
@@ -16,12 +44,19 @@ const AppNavBar = () => {
     <div className="border-bottom bg-white border-slate-200 text-black text-sm">
       <div className="container mx-auto flex justify-between items-center pt-1">
         <div className="flex items-center">
-          <Link href={"/published/" + surveyId} target="_blank">
-            <AppButton className="ma-0 mr-2">
-              <EyeIcon className="w-4 h-4 mr-1" />
-              Preview
-            </AppButton>
-          </Link>
+          <DialogComponent
+            trigger={
+              <AppButton className="ma-0 mr-2">
+                <EyeIcon className="w-4 h-4 mr-1" />
+                Preview
+              </AppButton>
+            }
+          >
+            <iframe
+              src={`/published/${surveyId}`}
+              className="w-full h-[calc(100vh_-_100px)] "
+            ></iframe>
+          </DialogComponent>
           <AppButton
             primary={true}
             className="ma-0 mr-2"
