7ea8210ca76489d07fc2455ff72b6c3d58aa4120
Wheelebin <jln.allen95@gmail.com>
2022-12-19 19:37:24 +0100

    Removed some unused stuff in surveys page

--- src/pages/surveys.tsx
+++ src/pages/surveys.tsx
@@ -2,16 +2,13 @@ import AppButton from "@/components/AppButton";
 import MainNavBar from "@/components/MainNavBar";
 import AppCard from "@/components/AppCard";
 import Link from "next/link";
-import { useSession, signIn, signOut } from "next-auth/react";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 import surveyApi from "@/api/survey";
 
 const Survey = () => {
-  const router = useRouter();
   const { data: surveys } = surveyApi.useGetAll();
 
-  const { data: session, status } = useSession({ required: true });
+  useSession({ required: true });
 
   return (
     <div className="bg-gray-100 h-screen">
